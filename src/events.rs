131ca59c36f66cafbde6e5118d2a3cdced1eac94
Midka <me@midka.dev>
2022-10-07 19:09:13 +0300

    BROKEN CODE

---
+++ src/events.rs
@@ -0,0 +1,62 @@
+#[async_trait]
+impl EventHandler for Handler {
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                "id" => commands::id::run(&command.data.options),
+                "attachmentinput" => commands::attachmentinput::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| commands::ping::register(command))
+                .create_application_command(|command| commands::id::register(command))
+                .create_application_command(|command| commands::welcome::register(command))
+                .create_application_command(|command| commands::numberinput::register(command))
+                .create_application_command(|command| commands::attachmentinput::register(command))
+        })
+        .await;
+
+        println!(
+            "I now have the following guild slash commands: {:#?}",
+            commands
+        );
+
+        let guild_command = Command::create_global_application_command(&ctx.http, |command| {
+            commands::wonderful_command::register(command)
+        })
+        .await;
+
+        println!(
+            "I created the following global slash command: {:#?}",
+            guild_command
+        );
+    }
+}

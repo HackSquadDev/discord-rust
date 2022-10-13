42b968b2c224ecc3e3bad92b48cc2468a36d4b78
Midka <me@midka.dev>
2022-10-13 22:23:02 +0300

    fix: clippy errors :)

--- src/utils/calculate_latency.rs
+++ src/utils/calculate_latency.rs
@@ -6,7 +6,5 @@ pub fn calculate_latency(start: DateTime<Utc>, end: DateTime<Utc>) -> i64 {
     let end_ts = end.timestamp();
     let end_ts_ss = end.timestamp_subsec_millis() as i64;
 
-    let time = ((end_ts - start_ts) * 1000) + (end_ts_ss - start_ts_ss);
-
-    time
+    ((end_ts - start_ts) * 1000) + (end_ts_ss - start_ts_ss)
 }

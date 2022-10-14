820da1f7ae764095567f370624085a62485f258a
Midka <me@midka.dev>
2022-10-14 18:45:58 +0300

    fix: move hero's bio to the top

--- src/commands/hero.rs
+++ src/commands/hero.rs
@@ -85,17 +85,17 @@ pub async fn run(
     }
 
     let data = format!(
-        "`ℹ️` **Information**\n<:reply_multi:1029067132572549142>**Name:** `{}`\n<:reply_multi:1029067132572549142>**Location:** `{}`\n<:reply_multi:1029067132572549142>**Bio:** {}\n<:reply_multi:1029067132572549142>**Total PRs:** `{}`\n<:reply:1029065416905076808>**Last Activity:** {}\n\n`📙` **Socials**\n<:gh:1029368861776167004> **GitHub:** https://github.com/{}\n<:lkdn:1029410421641326755> **LinkedIn:** {}\n<:twitter:1029410910432935936> **Twitter:** {}\n<:discord:1029412089170767922> **Discord:** {}\n\n`🔗` **Last 3 PRs**\n{}",
+        "{} `ℹ️` **Information**\n<:reply_multi:1029067132572549142>**Name:** `{}`\n<:reply_multi:1029067132572549142>**Location:** `{}`\n<:reply_multi:1029067132572549142>**Total PRs:** `{}`\n<:reply:1029065416905076808>**Last Activity:** {}\n\n`📙` **Socials**\n<:gh:1029368861776167004> **GitHub:** https://github.com/{}\n<:lkdn:1029410421641326755> **LinkedIn:** {}\n<:twitter:1029410910432935936> **Twitter:** {}\n<:discord:1029412089170767922> **Discord:** {}\n\n`🔗` **Last 3 PRs**\n{}",
+        format!("_\"{}\"_ \n\n", hero.bio.unwrap_or_else(|| "".to_string())),
         hero.name.unwrap_or_else(|| "Unknown".to_string()),
         hero.location.unwrap_or_else(|| "Unknown".to_string()),
-        hero.bio.unwrap_or_else(|| "Unknown".to_string()),
         if let Some(pulls) = hero.total_pulls {
             format!("{}", pulls)
         } else {
             "Not Available".to_string()
         },
         if let Some(last_activity_occurred_at) = hero.last_activity_occurred_at {
-            format!("<t:{}:F>", 
+            format!("<t:{}:F>",
                 Timestamp::from(last_activity_occurred_at).unix_timestamp()
             )
         } else {
